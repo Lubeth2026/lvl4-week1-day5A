a2e673161c532725d80f0ddcf5718c013586d60c
Lubeth <lubethjimenez@yahoo.com>
2026-06-11 21:49:50 -0400

    Add READ data from employees.json file created

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ employees = [
     { "name": "Kari Millan", "shift": "night", "working": False }
 ]
 
-with open("employees.json", "w") as file:
-  json.dump(employees, file)
+# with open("employees.json", "w") as file:
+#   json.dump(employees, file)
+
+with open("employees.json", "r") as file:
+    employees = json.load(file)
+print(employees)
