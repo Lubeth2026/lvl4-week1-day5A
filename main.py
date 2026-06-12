0d64b9422cfbbf5074f6ce8d3ba0d382d7abb816
Lubeth <lubethjimenez@yahoo.com>
2026-06-11 21:38:50 -0400

    Add JSON import, created a employees.json file & WRITE new data into file

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
-print("Greetings!")
+import json
+# print("Greetings!")
+
+employees = [
+    { "name": "Jess Noelle", "shift": "morning", "working": True },
+    { "name": "Joe Hadden", "shift": "night", "working": True },
+    { "name": "Kari Millan", "shift": "night", "working": False }
+]
+
+with open("employees.json", "w") as file:
+  json.dump(employees, file)
